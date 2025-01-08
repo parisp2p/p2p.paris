7b7322b62109455eb4953eb8fc3bf696476dd27c
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2025-01-08 13:13:31 +0545

    feat: talks page

---
+++ types/client.ts
@@ -0,0 +1,11 @@
+export interface ClientTalk {
+  slug: string;
+  startDateTime: string;
+  endDateTime: string;
+  language: string;
+  location: string;
+  speakers: string[];
+  imageURL: string;
+  title: string;
+  description: string;
+}
