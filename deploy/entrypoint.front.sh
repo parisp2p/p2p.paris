1b85229ba0e07b81f476a97b8857e6519fa531d0
pwnh4 (@loicttn) <loic@ttn.am>
2025-01-13 19:37:43 +0100

    feat: switch frontend to static build

--- deploy/entrypoint.front.sh
+++ deploy/entrypoint.front.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 yarn db:generate
+# requires connection to db, so cannot be done at build time
 yarn build
-yarn start
+yarn export
+
+cp /app/out /usr/share/nginx/html
+
+nginx -g 'daemon off;'
