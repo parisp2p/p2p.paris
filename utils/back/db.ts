30ecb91dafa07149f6810c0441b80e5a30dd7c95
pwnh4 <pwnh4@m1n1t3l.local>
2025-01-18 20:53:07 +0100

    fix: prisma singleton

--- utils/back/db.ts
+++ utils/back/db.ts
@@ -1,17 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientPropertyName = `__prevent-name-collision__prisma`;
-
-type GlobalThisWithPrismaClient = typeof globalThis & {
-  [prismaClientPropertyName]: PrismaClient;
-};
-
-const getPrismaClient = () => {
-  const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-  if (!newGlobalThis[prismaClientPropertyName]) {
-    newGlobalThis[prismaClientPropertyName] = new PrismaClient();
-  }
-  return newGlobalThis[prismaClientPropertyName];
-};
-
-export const db = getPrismaClient();
+export const db = new PrismaClient();
